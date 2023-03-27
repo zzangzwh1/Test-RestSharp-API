c2a00d23422982189acbcd7922bac40caff14b4f
Wonhyuk Cho <zzangzwh@hotmail.com>
2023-03-26 18:12:17 -0600

    RestSharp Test

---
+++ Test-RestSharp API/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_RestSharp_API
+{
+    public class Post
+    {
+        public string title { set; get; }
+        public string body { set; get; }
+        public int userid { set; get; }
+    }
+}
---
+++ Test-RestSharp API/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Test_RestSharp_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            var client = new RestClient(url);
+            var request = new RestRequest();
+
+            var body = new Post { title = "This is Test Body", body = "This is Test Title", userid = 2 };
+
+
+            request.AddJsonBody(body);
+
+            var response = client.Post(request);
+
+            Console.WriteLine($"{response.StatusCode.ToString()}     {response.Content.ToString()}" ) ;
+            Console.ReadLine();
+
+
+        }
+    }
+}
